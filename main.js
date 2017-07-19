abe36be3b93f2e1aae1ad1fdb1e86e6dc8075e01
Muhammad Rizwan <mrizwanghuman@yahoo.com>
2017-07-19 12:15:11 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function LetterCapitalize(str) {
+	// body...
+	if(isNaN(str)){
+	var strarray = str.split(" ");
+	var colling=[];
+
+	if(str.length=0){
+str.charAt(0).toUpperCase()+str.slice(1);
+console.log(str);
+	}else{
+	for (var i = 0; i < strarray.length; i++) {
+		colling.push(strarray[i].charAt(0).toUpperCase()+strarray[i].slice(1));
+	}
+	console.log(colling.join(" "));
+}
+}else{
+
+	alert("Only string accepted");
+}
+}
+
+LetterCapitalize("the");
